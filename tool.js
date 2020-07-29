77b56aa836962f92449e2f6cf81798cf9f3573ab
Émile Perron <contact@emileperron.com>
2020-07-28 21:41:02 -0400

    Created basic structure with empty tool template

---
+++ tool.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Tool {
+    constructor() {
+        // set up testing variables and connections, if needed
+    }
+
+    run() {
+        // runs the tool on the page/website
+    }
+
+    get results() {
+        // returns an array of formatted Result objects
+    };
+
+    cleanup() {
+        // cleans up the variables and connections
+    };
+}
+
+module.exports = Tool;
